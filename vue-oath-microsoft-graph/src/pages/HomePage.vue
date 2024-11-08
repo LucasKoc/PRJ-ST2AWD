539c0a476a09cf82c5495dad203e67837bc6b928
Lucas <45604254+LucasKoc@users.noreply.github.com>
2024-11-08 18:33:46 +0100

    Update HomePage.vue

--- vue-oath-microsoft-graph/src/pages/HomePage.vue
+++ vue-oath-microsoft-graph/src/pages/HomePage.vue
@@ -1,12 +1,11 @@
 <script lang="ts">
-import { defineComponent, computed} from "vue";
+import { defineComponent, computed } from "vue";
 import { useStore } from "vuex";
+import SignInButton from "@/components/SignInButton.vue";
 
 export default defineComponent({
   name: "HomePage",
-  components: {
-  },
-
+  components: {SignInButton},
   setup() {
     const store = useStore();
     const user = computed(() => store.state.user);
@@ -18,10 +17,30 @@ export default defineComponent({
 });
 </script>
 
+
 <template>
-  <div>
-    <h1>HomePage.vue</h1>
-    <p>Welcome {{ user }}</p>
+  <!-- Set content at the middle of the page -->
+  <div class="m-8 flex items-center justify-center h-full">
+    <div class="relative z-10 text-center">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hello {{ user ? user.username : 'there' }}!</h1>
+        <p class="mb-5 text-lg">
+          Welcome to the PRJ-ST2AWD!
+        </p>
+        <p class="mb-5 text-lg">
+          This is a simple web application that demonstrates how to use Vue.js and ClimatiQ API.
+        </p>
+        <p v-if="!user" class="mb-5 text-lg">
+          Please connect with your Microsoft/Google account to get started.
+        </p>
+        <p v-else class="mb-5 text-lg">
+          You are now connected with your Microsoft/Google account. You can access to functionalities in the navigation bar.
+        </p>
+      </div>
+      <div v-if="!user">
+        <SignInButton color="primary" icon="user">Sign in</SignInButton>
+      </div>
+    </div>
   </div>
 </template>
 
